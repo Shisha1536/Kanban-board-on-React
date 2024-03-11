77d8fc7018ebf0f7635a055ef9ebda75ce28a879
Shisha1536 <schischkoff2017@yandex.ru>
2024-03-11 16:07:36 +0300

    components

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import "./calculation-of-tasks/calculationTasks";
+import './footer.css';
+
+export function Basement() {
+    return (
+        <div className="footer__block">
+            Тест
+        </div>
+      );
+}
